e1fcb5a16eee3ce10c49409e5b56c0d707b59584
Eugen Paval <epaval@hotmail.com>
2020-02-02 19:19:14 -0500

    Another Kata solved

--- pythonKata/SquaresInSquare.py
+++ pythonKata/SquaresInSquare.py
@@ -1,26 +1,32 @@
+def getUniqueSquares(square):
+    if square == 0:
+        return set()
+
+    n = int(square**.5)
+    for k in range(n, 0, -1):
+        diff = square
+        m = k
+        while diff > 0:
+            diff -= m**2
+            candidates = getUniqueSquares(diff)
+            candidates.add(m)
+            
+            sumOfSquares = sum(map(lambda x: x**2, candidates))
+            if sumOfSquares == square:
+                return candidates
+
+            m = int(diff**.5)
+
+
+    return set()
+
 def decompose(n):
-    for x in range(n-1, 0, -1):
-        result = list(getPossibleNumbers(n**2, x))
-        result.sort()
-
-        if result == []:
-            break
-
-        sumSquares = sum(map(lambda x: x**2, result))
-        if sumSquares == n ** 2:
-            return result
+    for x in range(n-1, 1, -1):
+        f = getUniqueSquares(n**2 - x**2)
+        if len(f) > 0 and x not in f:
+            f.add(x)
+            r = list(f)
+            r.sort()
+            return r
 
     return None
-
-def getPossibleNumbers(square, candidate):
-    if candidate == 0:
-        return set()
-    if candidate == 1:
-        return set([1])
-
-    diff = square - candidate**2
-    nc = int(diff**.5)
-    next = getPossibleNumbers(diff, nc)
-    next.add(candidate)
-
-    return next
--- pythonKata/test_SquaresInSquare.py
+++ pythonKata/test_SquaresInSquare.py
@@ -10,3 +10,9 @@ class test_SquareInSquares(unittest.TestCase):
 
     def test11(self):
         self.assertEqual(sq.decompose(11), [1,2,4,10])
+
+    def test50(self):
+        self.assertEqual(sq.decompose(50), [1, 3, 5, 8, 49])
+
+    def test625(self):
+        self.assertEqual(sq.decompose(625), [2, 5, 8, 34, 624])
