10ab371b218f9c1be70c3b50c4332bc3c44fd761
Eugen Paval <epaval@wedenprime.com>
2020-03-03 18:38:16 -0500

    Working on TheLyft

--- PythonKata/TheLift.py
+++ PythonKata/TheLift.py
@@ -1,11 +1,49 @@
 #TheLift.py
+from bisect import bisect_right, bisect_left
+from collections import deque
+
 class Elevator(object):
     capacity: int
     destinations: [int]
     direction: str
+    currentFloor: int
 
     def __init__(self, capacity):
         self.capacity = capacity
+        self.direction = "UP"
+        self.currentFloor = 0
+
+    def nextFloorsStop(self):
+        if self.direction =="UP": 
+            floors = [f for f in self.destinations if f > self.currentFloor]
+        else:
+            floors = [f for f in reversed(self.destinations) if f < self.currentFloor]
+        
+        return deque(floors)
+
+    def goUp(self):
+        self.direction = "UP"
+
+    def goDown(self):
+        self.direction = "DOWN"
+
+    def addPeopleToElevator(self, queue):
+        if self.direction == "UP":
+            queue = [f for f in queue if f > self.currentFloor]
+        else:
+            queue = [f for f in queue if f < self.currentFloor]
+
+        while len(queue) > 0 and self.capacity > len(self.destinations):
+            bisect_right(self.destinations, queue[0])
+            del queue[0]
+
+    def remPeopleFromElevator(self):
+        for d in self.destinations:
+            if d == self.currentFloor:
+                del d
+
+    def stopAtFloor(self, floor):
+        self.currentFloor = floor
 
 class Floors(object):
     queue: [[int]]
@@ -20,7 +58,7 @@ class Building(object):
 
 class Dinglemouse(object):
     def __init__(self, queues, capacity):
-        pass
+        self.building = Building(queues, capacity)
         
     def theLift(self):
         return []
