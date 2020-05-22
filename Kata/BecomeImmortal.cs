11d0c2ef52dc81de040d80aed3eba8927e5b50f3
Eugen Paval <eugen.paval@gmail.com>
2020-05-22 11:44:07 -0400

    Added BecomeImmortal; cleaned up non CS projects

---
+++ Kata/BecomeImmortal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Numerics;
+
+namespace Kata
+{
+    public static class Immortal
+    {
+        /// set true to enable debug
+        public static bool Debug = false;
+
+        public static long ElderAge(long n, long m, long k, long newp)
+        {
+            return Helper(new BigInteger(n), new BigInteger(m), new BigInteger(k), newp);
+        }
+
+        public static long Helper(BigInteger n, BigInteger m, BigInteger k, long newp)
+        {
+            Func<BigInteger, BigInteger> p2 = x =>
+            {
+                var result = 1L;
+                while (result < x)
+                    result <<= 1;
+
+                return result;
+            };
+
+            Func<BigInteger, BigInteger, BigInteger> sumInterval = (lowerB, upperB) => (lowerB + upperB) * (upperB - lowerB + 1) / 2;
+            Func<BigInteger, BigInteger, BigInteger> max = (x, y) => x >= y ? x : y;
+
+            if (n == 0 || m == 0)
+                return 0;
+
+            if (n > m)
+                (n, m) = (m, n);
+
+            var pn = p2(n);
+            var pm = p2(m);
+
+            if (k > pm)
+                return 0;
+
+            BigInteger life = 0;
+            if (pn == pm)
+                life = sumInterval(1, pm - k - 1) * (n + m - pm) + Helper(pm - m, pn - n, k, newp);
+
+            if (pn < pm)
+            {
+                pn = pm / 2;
+                life = n * sumInterval(1, pm - k - 1) - (pm - m) * sumInterval(max(0, pn - k), pm - k - 1);
+
+                if (k <= pn)
+                    life += (pn - k) * (pn - n) * (pm - m) + Helper(pn - n, pm - m, 0, newp);
+                else
+                    life += Helper(pn - n, pm - m, k - pn, newp);
+            }
+
+            var result = (long)(life % newp);
+            return result < 0 ? newp + result : result;
+        }
+    }
+}
---
+++ KataTests/BecomeImmortalTests.cs
@@ -0,0 +1,22 @@
+﻿using Kata;
+using NUnit.Framework;
+
+[TestFixture]
+public class ImmortalTest
+{
+
+    [Test]
+    public void example()
+    {
+        //Assert.AreEqual((long)5, Immortal.ElderAge(8, 5, 1, 100));
+        //Assert.AreEqual((long)224, Immortal.ElderAge(8, 8, 0, 100007));
+        //Assert.AreEqual((long)11925, Immortal.ElderAge(25, 31, 0, 100007));
+        //Assert.AreEqual((long)4323, Immortal.ElderAge(5, 45, 3, 1000007));
+        //Assert.AreEqual((long)1586, Immortal.ElderAge(31, 39, 7, 2345));
+        //Assert.AreEqual((long)808451, Immortal.ElderAge(545, 435, 342, 1000007));
+        // You need to run this test very quickly before attempting the actual tests :)
+        //Assert.AreEqual((long)5456283, Immortal.ElderAge(28827050410L, 35165045587L, 7109602, 13719506));
+
+        Assert.AreEqual((long) 79, Immortal.ElderAge(39, 557, 16, 328));
+    }
+}
